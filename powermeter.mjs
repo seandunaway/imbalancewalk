827a3a5c376d5c53b366d0bc76a996fef4912310
seandunaway <git@seandunaway.com>
2023-11-01 21:34:46 -0700

    powermeter

---
+++ powermeter.mjs
@@ -0,0 +1,51 @@
+export function create(options) {
+    let powermeter = {
+        canvas: undefined,
+        context: undefined,
+
+        label: undefined,
+        label_xy: [0, 0],
+
+        max: 60,
+        min: 40,
+        step: 1,
+        value: 50,
+
+        flip: false,
+        hover_line: true,
+        hover_label: true,
+
+        color_background: '#ffd6d6',
+        color_value: '#e4ffd9',
+        color_text: 'white',
+        color_text_background: '#44475a',
+
+        on_click,
+        on_hover,
+
+        ...options
+    }
+
+    init(powermeter)
+    return powermeter
+}
+
+export function init(powermeter) {
+    if (!powermeter.canvas) throw new Error('canvas')
+    if (!powermeter.context) powermeter.context = powermeter.canvas.getContext('2d')
+    powermeter.canvas.style.backgroundColor = powermeter.color_background
+}
+
+export function draw(powermeter) {
+    powermeter.context.fillStyle = powermeter.color_value
+    powermeter.context.clearRect(0, 0, powermeter.canvas.width, powermeter.canvas.height)
+    powermeter.context.fillRect(0, powermeter.canvas.height, powermeter.canvas.width, -(powermeter.canvas.height / 2))
+}
+
+export function on_click(powermeter) {
+
+}
+
+export function on_hover(powermeter) {
+
+}
--- script.mjs
+++ script.mjs
@@ -1,2 +1,12 @@
 import * as elements from './elements.mjs'
+import * as powermeter from './powermeter.mjs'
 
+let rs = powermeter.create({canvas: elements.canvas.rs})
+let is = powermeter.create({canvas: elements.canvas.is})
+let ib = powermeter.create({canvas: elements.canvas.ib})
+let rb = powermeter.create({canvas: elements.canvas.rb})
+
+powermeter.draw(rs)
+powermeter.draw(is)
+powermeter.draw(ib)
+powermeter.draw(rb)
