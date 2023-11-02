63b4d7b3f40ba3b3ed72d628c946812a574d9ce4
seandunaway <git@seandunaway.com>
2023-11-01 21:07:56 -0700

    simplify canvas

--- elements.mjs
+++ elements.mjs
@@ -1,9 +1,8 @@
 export let h1 = document.querySelector('h1')
 
-export let contexts = {}
-export let canvases = document.querySelectorAll('canvas')
-for (let canvas of canvases) {
-    contexts[canvas.id] = canvas.getContext('2d')
+export let canvas = {}
+for (let element of document.querySelectorAll('canvas')) {
+    canvas[element.id] = element
 }
 
 export let p = document.querySelector('p')
