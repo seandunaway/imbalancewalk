a9ac4ab8cc8fe866af579f76968ae4c16eb9f22d
seandunaway <git@seandunaway.com>
2023-11-03 19:21:07 -0700

    quote

---
+++ quote.mjs
@@ -0,0 +1,35 @@
+export function create(options) {
+    let quote = {
+        value: 5000.00,
+        delay: 100,
+        tick: 0.25,
+        rs: 50,
+        is: 50,
+        ib: 50,
+        rb: 50,
+    }
+
+    return quote
+}
+
+export function update(quote) {
+    // is there a lift by an initiative buyer?
+    if (coinflip(quote.ib))
+        // is it not held by a responsive seller?
+        if (!coinflip(quote.rs))
+            quote.value += quote.tick
+
+    // is there a hit by an initiative seller?
+    if (coinflip(quote.is))
+        // is it not held by a responsive buyer?
+        if (!coinflip(quote.rb))
+            quote.value -= quote.tick
+}
+
+export async function delay(quote) {
+    await new Promise(resolve => setTimeout(resolve, quote.delay))
+}
+
+export function coinflip(probability = 50) {
+    return Math.random() < (probability / 100)
+}
--- script.mjs
+++ script.mjs
@@ -1,17 +1,29 @@
 import * as elements from './elements.mjs'
 import * as powermeter from './powermeter.mjs'
+import * as quote from './quote.mjs'
+
+let q = quote.create()
 
 let rs = powermeter.create({canvas: elements.canvas.rs, label: 'rs↓', flip: true})
 let is = powermeter.create({canvas: elements.canvas.is, label: 'is↓', flip: true})
 let ib = powermeter.create({canvas: elements.canvas.ib, label: 'ib↑'})
 let rb = powermeter.create({canvas: elements.canvas.rb, label: 'rb↑'})
 
-function frame() {
+async function frame_quote() {
+    if (!elements.h1) return
+    quote.update(q)
+    elements.h1.textContent = q.value.toFixed(2)
+    await quote.delay(q)
+    requestAnimationFrame(frame_quote)
+}
+
+async function frame_powermeter() {
     powermeter.draw(rs)
     powermeter.draw(is)
     powermeter.draw(ib)
     powermeter.draw(rb)
-
-    requestAnimationFrame(frame)
+    requestAnimationFrame(frame_powermeter)
 }
-requestAnimationFrame(frame)
+
+frame_quote()
+frame_powermeter()
