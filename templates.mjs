0e321b4a12ab69c19cf3765fafdbb74f13a2505f
seandunaway <git@seandunaway.com>
2023-11-04 07:57:24 -0700

    colon replacement

--- templates.mjs
+++ templates.mjs
@@ -1,9 +1,9 @@
 export function p(objects) {
     return `
-        rb:<b>${objects.rb.value}</b>%
-        ib:<b>${objects.ib.value}</b>%
-        is:<b>${objects.is.value}</b>%
-        rs:<b>${objects.rs.value}</b>%
-        s:<b>${objects.q.speed}</b>x
+        rb·<b>${objects.rb.value}</b>%
+        ib·<b>${objects.ib.value}</b>%
+        is·<b>${objects.is.value}</b>%
+        rs·<b>${objects.rs.value}</b>%
+        s·<b>${objects.q.speed}</b>x
     `
 }
