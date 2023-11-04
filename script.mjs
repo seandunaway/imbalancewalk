51030e3448cd0315f6575b7a3d94f2102a3b25a2
seandunaway <git@seandunaway.com>
2023-11-03 19:32:22 -0700

    wire values

--- script.mjs
+++ script.mjs
@@ -10,9 +10,21 @@ let ib = powermeter.create({canvas: elements.canvas.ib, label: 'ib↑'})
 let rb = powermeter.create({canvas: elements.canvas.rb, label: 'rb↑'})
 
 async function frame_quote() {
-    if (!elements.h1) return
+    if (!elements.h1 || !elements.p) return
+
+    q.rs = rs.value
+    q.is = is.value
+    q.ib = ib.value
+    q.rb = rb.value
     quote.update(q)
+
     elements.h1.textContent = q.value.toFixed(2)
+    elements.p.innerHTML = `\
+        rs:<b>${rs.value}</b>
+        is:<b>${is.value}</b>
+        ib:<b>${ib.value}</b>
+        rb:<b>${ib.value}</b>`
+
     await quote.delay(q)
     requestAnimationFrame(frame_quote)
 }
@@ -22,6 +34,7 @@ async function frame_powermeter() {
     powermeter.draw(is)
     powermeter.draw(ib)
     powermeter.draw(rb)
+
     requestAnimationFrame(frame_powermeter)
 }
 
